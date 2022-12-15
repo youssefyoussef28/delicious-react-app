0a49eb508f8d8c08115c4666f55e3a5fd2e1ed8f
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-15 13:15:04 +0200

    Veggies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Pages from "./pages/Pages";
+import Category from "./pages/Category";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Category />
       <Pages />
     </div>
   );
