817053c09398e4db94ceacf5de34d9866dc13fa0
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-17 15:56:08 +0200

    Added the recipes component and active class and setinner HTML

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function Recipe() {
+  let params = useParams();
+  const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("instructions");
+
+  const fetchDetails = async (id) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails(params.id);
+  }, [params.id]);
+
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </Button>
+        <div>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+        </div>
+      </Info>
+    </DetailWrapper>
+  );
+}
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+
+  img {
+    border-radius: 0.5rem;
+  }
+
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+    font-weight: 400;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  margin: 0.5rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 300;
+`;
+const Info = styled.div`
+  margin-left: 10rem;
+`;
+
+export default Recipe;
