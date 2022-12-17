662fcde5913da51009e6064affc7aa988a4dceca
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-17 16:13:23 +0200

    Conditional rendering of the instructions and the components

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -39,9 +39,19 @@ function Recipe() {
         >
           Ingredients
         </Button>
-        <div>
-          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
-        </div>
+        {activeTab === "ingredients" && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => {
+              return <li key={ingredient.id}>{ingredient.original}</li>;
+            })}
+          </ul>
+        )}
+        {activeTab === "instructions" && (
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </div>
+        )}
       </Info>
     </DetailWrapper>
   );
