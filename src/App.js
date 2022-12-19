1be598a73990c448580d228a53763f80c1c7fa8d
Youssef Youssef <youssefyoussef28@gmail.com>
2022-12-19 19:12:25 +0200

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Pages from "./pages/Pages";
 import Category from "./pages/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>Delicious</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +22,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  padding: 0.2rem;
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -4,15 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cuisine/:type" element={<Cuisine />} />
-      <Route path="/searched/:search" element={<Searched />} />
-      <Route path="/recipe/:id" element={<Recipe />} />
-    </Routes>
+    <AnimatePresence exitBeforeEnter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
